ffe17b57567330f468bea18182e387e460aff40f
NESHGP04 <marines.garcia2004@gmail.com>
2025-09-03 13:06:44 -0600

    Logo +searchbar Navbar

--- Dashboard/src/components/navbar/Navbar.jsx
+++ Dashboard/src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
-import { IoMdClose } from "react-icons/io";
+import { IoSearchSharp } from "react-icons/io5";
 import "@styles/Navbar.css";
 import logo from "@assets/logo.png"
 import Sidebar from "./SideNavbar"
@@ -23,15 +23,32 @@ const Navbar = () => {
             /> 
           </div>
 
-          <div className="nav-item">Searchbar</div>
-          <div className="nav-item">User Photo</div>
+          <div className="nav-item">
+            <div className="sidebar-header">
+                <img src={logo} alt="logo" className="logo" />
+                <h1 className="logo-text">
+                    <span className="logo-im">IM</span>
+                    <span className="logo-progress">PROGRESS</span>
+                </h1>
+            </div>
+          </div>
+
+          <div className="nav-item searchbar-container">
+            <input 
+                type="text" 
+                placeholder="Buscar" 
+                className="searchbar"
+            /><IoSearchSharp className="icon"/>
+          </div>
+
+          {/* <div className="nav-item">User Photo</div>
           <div className="nav-item">Name</div>
           <div className="nav-item">Profile</div>
-          <div className="nav-item">Notifications</div>
+          <div className="nav-item">Notifications</div> */}
         </nav>
       </header>
 
-      {/* Side Navbar SEPARAR A OTRO COMPONENTE DESP*/}
+      {/* Sidebar*/}
       <Sidebar showNav={showNav} setShowNav={setShowNav} />
     </div>
   );
